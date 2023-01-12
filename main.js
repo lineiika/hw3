4b944ad38fe0cf48d9c6ea66218229cfbc10d860
lineiika <avramenko.liana@gmail.com>
2023-01-12 17:38:36 +0200

    add final commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let userAnswer = Number(prompt('Enter your number'));
 let userAnswer2 = Number(prompt('Enter your number'));
 let userAnswer3 = Number(prompt('Enter your number'));
-let result = (userAnswer + userAnswer2 +userAnswer3)/3
+let result = (userAnswer + userAnswer2 +userAnswer3)/3;
 
-alert(result)
+alert(result);
